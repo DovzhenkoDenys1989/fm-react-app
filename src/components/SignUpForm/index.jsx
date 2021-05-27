221f06ec52f9286591e720885f698d82e2036b62
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-28 00:38:19 +0300

    Add password validation example

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -8,10 +8,6 @@ const initialValues = {
   lastname: "",
   email: "",
   password: "",
-  isfirstnameValid: true,
-  islastnameValid: true,
-  isemailValid: true,
-  ispasswordValid: true,
 };
 
 class SignUpForm extends Component {
@@ -30,37 +26,30 @@ class SignUpForm extends Component {
     this.setState({ [name]: value, [`is${name}Valid`]: !value.includes(" ") });
   };
 
-  firstnameChange() {
-    
+  validateName = (value) => {
+    return true;
   }
-  lastnameChange() {
-    
+  validateEmail = (value) => {
+    return true;
   }
-  emailChange() {
-    
-  }
-  passwordChange({ target: { value } }) {
-    const isValid = value.length >= 8;
-    this.setState({password: value, ispasswordValid: isValid });
-  }
-
+  validatePassword = (value) => {
+    const regex = /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{6,}/g
+    return value.length === 0 || regex.test(value);
+  };
+  
   render() {
     const {
       firstname,
       lastname,
       email,
       password,
-      isemailValid,
-      isfirstnameValid,
-      islastnameValid,
-      ispasswordValid,
     } = this.state;
     return (
       <form className={style.container} onSubmit={this.submitHandler}>
-        <FormInput value={firstname} onChange={} isValid={isfirstnameValid} placeholder="Введите имя" />
-        <FormInput value={lastname} onChange={} isValid={islastnameValid} placeholder="Введите фамилию" />
-        <FormInput value={email} onChange={} isValid={isemailValid} placeholder="Введите email" type="email" />
-        <FormInput value={password} onChange={this.passwordChange} isValid={ispasswordValid} placeholder="Введите пароль" type="password" />
+        <FormInput name="firstname" value={firstname} onChange={this.handleChange} validationHandler={this.validateName} placeholder="Введите имя" />
+        <FormInput name="lastname" value={lastname} onChange={this.handleChange} validationHandler={this.validateName} placeholder="Введите фамилию" />
+        <FormInput name="email" value={email} onChange={this.handleChange} validationHandler={this.validateEmail} placeholder="Введите email" type="email" />
+        <FormInput name="password" value={password} onChange={this.handleChange} validationHandler={this.validatePassword} placeholder="Введите пароль" type="password" />
         <input className={style.input} type="submit" />
       </form>
     );
@@ -71,5 +60,6 @@ export default SignUpForm;
 
 /*
   1. Написать функции-обработчики изменений имени, фамилии, email.
-  2. Добавить валидацию этих полей.
+  2. Реализовать удаление пробелов во всех полях.
+  3. Добавить валидацию этих полей.
 */
