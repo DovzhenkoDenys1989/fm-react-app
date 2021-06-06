3f5824fc654a0e764e94ef51a6412a85b075882b
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-06-06 18:45:53 +0300

    Add routing example

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
 import './App.css';
 import React, { useState } from 'react';
+import { BrowserRouter, Switch, Route, Link, Router } from 'react-router-dom';
 import UserLoader from './components/UserLoader';
 import WindowSizes from './components/WindowSizes';
 import Counter from './components/PracticeCounter/Counter';
 
-const usersDB = [
-  {
-    id: 1,
-    firstname: 'John',
-    lastname: 'Smith',
-  },
-  {
-    id: 2,
-    firstname: 'Bob',
-    lastname: 'Brown',
-  },
-  {
-    id: 3,
-    firstname: 'Sara',
-    lastname: 'Smith',
-  },
-  {
-    id: 4,
-    firstname: 'Mary',
-    lastname: 'Black',
-  },
-];
-
-/*
-  Перепишите Aloha, App как функциональные компоненты
-*/
-
 function App (props) {
   return (
-    <>
-      {/*<WindowSizes />
-      <UserLoader /> */}
-      <Counter />
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/contacts'>Contacts</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/contacts'>
+          <Contacts />
+        </Route>
+        <Route path='/about' component={About} />
+        <Route path='*' component={NotFound}>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
+
+const Home = () => <div>HOME</div>;
+const About = props => {
+  console.log('ABOUT: ', props);
+  
+  setTimeout(() => {
+    props.history.push('/');
+  }, 5000);
+
+  return <div>About</div>;
+};
+const Contacts = props => {
+  console.log('CONTACTS: ', props);
+  return <div>Contacts</div>;
+};
+
+const NotFound = () => <div>Error 404. Page not found.</div>
 
 export default App;
